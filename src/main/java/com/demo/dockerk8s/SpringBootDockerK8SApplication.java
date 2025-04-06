bfe12b1e3bf3dabe28814a9cfb3ea1c26b22d880
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-04-06 21:02:04 +0530

    Created spring boot application to learn docker and kubernetes

---
+++ src/main/java/com/demo/dockerk8s/SpringBootDockerK8SApplication.java
@@ -0,0 +1,11 @@
+package com.demo.dockerk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerK8SApplication {
+    public static void main(String... args) {
+        SpringApplication.run(SpringBootDockerK8SApplication.class);
+    }
+}
