8090e6087ff1ca3b647b2eddc781437ca71787cb
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-08-05 13:06:10 +0530

    TestController.java  - Added timestamp log for API call

--- src/main/java/com/demo/dockerk8s/controller/TestController.java
+++ src/main/java/com/demo/dockerk8s/controller/TestController.java
@@ -3,11 +3,14 @@ package com.demo.dockerk8s.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class TestController {
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
+        System.out.println("Test API call at - " + LocalDateTime.now());
         return "Test API call.";
     }
 }
